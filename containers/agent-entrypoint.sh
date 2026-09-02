09ce5faeb2374f390bc12e2ef134cd56f68b50a2
Loylex <loylex@users.noreply.github.com>
2026-09-02 21:19:25 +0000

    Revert "chore: schedule pumpkin Telegram message"
    This reverts commit 71f68120901d260baa90b82d60b88cd0d56afaf9.

--- containers/agent-entrypoint.sh
+++ containers/agent-entrypoint.sh
@@ -60,7 +60,8 @@ forward_signal() {
 trap cleanup EXIT
 trap 'forward_signal' INT TERM
 if command -v crond >/dev/null 2>&1 && command -v sudo >/dev/null 2>&1 \
-  && [[ -f /etc/cron.d/loylex-pumpkin ]]; then
+  && [[ -x /usr/local/bin/loylex-dm-overview ]] \
+  && [[ -f /etc/cron.d/loylex-dm-overview ]]; then
   sudo -n crond -n -s >/dev/null 2>&1 &
   cron_pid=$!
 fi
